204932d504cae8794e47b23c5c19b74259f6cbd7
RealSpiderParker <realspiderparker18627@gmail.com>
2025-09-04 02:53:09 -0500

    testing git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/gitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/gitTest.java
@@ -2,4 +2,5 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 
 public class gitTest {
     //this is just a test, not to be used for code
+    //i dont know why it doesnt wanna push ? can i commit and show it on github?
 }
