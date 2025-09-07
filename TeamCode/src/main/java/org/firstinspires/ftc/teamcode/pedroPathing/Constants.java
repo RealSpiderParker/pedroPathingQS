9b7b5f57f7a59386a8123b64c828d0934034a4e7
RealSpiderParker <realspiderparker18627@gmail.com>
2025-09-06 22:43:22 -0500

    importing constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -5,19 +5,46 @@ import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.Mecanum;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(11.30);
-    public static MecanumConstants driveConstraints = new MecanumConstants();
+    public static MecanumConstants driveConstraints = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("fRight")
+            .rightRearMotorName("bRight")
+            .leftRearMotorName("bLeft")
+            .leftFrontMotorName("fLeft")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .rightFrontMotorName("fRight")
+            .rightRearMotorName("bRight")
+            .leftRearMotorName("bLeft")
+            .leftFrontMotorName("fLeft")
+            .leftFrontEncoderDirection(Encoder.FORWARD)
+            .leftRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD)
+            .robotWidth(17.625)
+            .robotLength(15.0);
+
     //yourHero's robot stuff ^^
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstraints)
+                .driveEncoderLocalizer(localizerConstants)
                 .build();
     }
 }
